9cbd6c4172b25282adf612f5e6c4f9333e66a594
Rayane-Hamani <rayanehamani.pro@gmail.com>
2021-10-21 23:14:34 +0200

    quelques ajouts en plus

--- src/main/java/univ/lille/gl/sra1/model/Employee.java
+++ src/main/java/univ/lille/gl/sra1/model/Employee.java
@@ -1,6 +1,10 @@
 package univ.lille.gl.sra1.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Employee {
 	private String firstname;
 	private String lastname;
+	private String password;
 }
