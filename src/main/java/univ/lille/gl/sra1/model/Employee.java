8020da243c6a9be0d4ccda9b333c46a035dad8d2
RazineHallil <razine.hallil.etu@univ-lille.fr>
2021-10-23 16:34:53 +0200

    Debug : Ajout id employee pour pouvoir tester

--- src/main/java/univ/lille/gl/sra1/model/Employee.java
+++ src/main/java/univ/lille/gl/sra1/model/Employee.java
@@ -1,14 +1,26 @@
 package univ.lille.gl.sra1.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Employee {
+	
+	@Id
+    @GeneratedValue
+	private long id;
+	
 	private String firstname;
 	private String lastname;
 	private String password;
 	
-	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public String getFirstname() {
 		return firstname;
 	}
